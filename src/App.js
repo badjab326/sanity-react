1e94d2ae0c8141864f82dd29610f0378209faffa
“julian <badjab326@gmail.com>
2022-10-26 13:53:14 -0700

    filled out components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
-import AllPosts from "./components/AllPosts"
-import OnePost from "./components/OnePost"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AllPosts from "./components/AllPosts.js";
+import OnePost from "./components/OnePost.js";
 
 function App() {
   return (
-    <BrowserRouter>
       <div>
-        <Route component={AllPosts} path="/" exact />
-        <Route component={OnePost} path="/:slug" />
+      <Routes>
+        <Route exact path="/" element={<AllPosts/>}/>
+        <Route path="/:slug" element={<OnePost/>}/>
+      </Routes>
       </div>
-    </BrowserRouter>
   );
 }
-
 export default App;
